8f3c5d26f93e570eaa6a409014b439260f490b94
akshayMore2018 <37394442+akshayMore2018@users.noreply.github.com>
2018-04-23 05:48:36 +0530

    Update EntityCreator.java

--- entities/EntityCreator.java
+++ entities/EntityCreator.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.entities;
 
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
+import com.mygdx.game.systems.CameraSystem;
 import com.mygdx.game.systems.CollisionSystem;
 import com.mygdx.game.systems.MotionSystem;
 import com.mygdx.game.systems.RenderSystem;
@@ -15,22 +16,25 @@ public class EntityCreator
     private MotionSystem motion;
     private UserInputSystem command;
     private CollisionSystem collisionCheck;
+    private CameraSystem camera;
 
     public EntityCreator(TileMap map)
     {
         ArrayList<Entity> entityList = new ArrayList<Entity>();
         entityList.add(new Player(map));
         entityList.add(new Zombie(map));
-        rend = new RenderSystem(entityList);
+        rend = new RenderSystem(entityList,map);
         motion = new MotionSystem(entityList);
         command=new UserInputSystem();
         collisionCheck=new CollisionSystem(entityList,map);
+        camera=new CameraSystem();
     }
 
     public void update()
     {
         collisionCheck.update();
         motion.update();
+        camera.update();
         command.Update();
     }
 
